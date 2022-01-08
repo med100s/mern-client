9dc694e1b2ab9c98b8da7ef0e5b8f5128cabd2da
med100s <alphawarrior999@gmail.com>
2022-01-09 02:16:10 +0500

    contaccts god demonstration 1/2

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,23 +2,30 @@ import React, { useState } from "react"
 import axios from 'axios'
 
 function Contacts(props){
-const [students,setStudents] = useState('')
+const [contacts,setContacts] = useState('')
 const [name,setName] = useState('')
 
 const handleClick = async() => {
     const api = `http://localhost:4000/api/contacts`
     const data = await axios.get(api, { headers: {"Authorization" : `Bearer ${props.token}`} })
-    setStudents(data)
+    setContacts(data)
 }
 
-console.log(students)
+console.log(contacts)
 
 return (
     <div className="container">
         <h2>Example component</h2>
-        <button onClick = {handleClick}>Get students</button>
+        <button onClick = {handleClick}>Get contacts</button>
         <div>
-            {JSON.stringify(students.data)}
+            {
+                contacts.data?contacts.data.map(el=> (
+                    <div>
+                        {el.status}
+                    </div>
+                ))
+                :null
+            }
         </div>
     </div>
 );
