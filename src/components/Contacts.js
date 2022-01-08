14c96c01070d0d50481a2bab66660087cf1d0622
med100s <alphawarrior999@gmail.com>
2022-01-09 04:54:51 +0500

    some changes

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,11 +3,26 @@ import axios from 'axios'
 
 function Contacts(props){
 const [contacts,setContacts] = useState('')
-const [name,setName] = useState('')
+const [contacId,setContactId] = useState('')
 
 const handleClick = async() => {
     const api = `http://localhost:4000/api/contacts`
     const data = await axios.get(api, { headers: {"Authorization" : `Bearer ${props.token}`} })
+    setContacts(data)
+}
+const handleDelete = async(id) => {
+    axios.delete(`http://localhost:4000/api/contacts/${id}`, 
+        { headers: {"Authorization" : `Bearer ${props.token}`} })
+    const data = await axios.get(`http://localhost:4000/api/contacts/`, 
+        { headers: {"Authorization" : `Bearer ${props.token}`} })
+    setContacts(data)
+}
+const handleCreate = async() => {
+    axios.post(`http://localhost:4000/api/contacts`, 
+        {"status":"govno"},
+        { headers: {"Authorization" : `Bearer ${props.token}`}, })
+    const data = await axios.get(`http://localhost:4000/api/contacts/`, 
+        { headers: {"Authorization" : `Bearer ${props.token}`} })
     setContacts(data)
 }
 
@@ -17,12 +32,19 @@ return (
     <div className="container">
         <h2>Example component</h2>
         <button onClick = {handleClick}>Get contacts</button>
+        
+        <button onClick = {handleCreate}>create contact</button>
         <div>
             {
-                contacts.data?contacts.data.map(el=> (
+                contacts.data?contacts.data.map(el => (
                     <div>
-                        {el.status}
+                        <div>
+                            {el.status} 
+                            {el._id} 
+                            <button onClick = {()=>handleDelete(el._id)}>delete contact</button>
+                        </div>
                     </div>
+                    
                 ))
                 :null
             }
